f7c5aa15d85d0e6a0cd969bca441f43692a23efc
Liberty Ebikade <ebikadeliberty7@gmail.com>
2024-06-11 14:15:44 +0100

    committed constant  in js

---
+++ constant.js
@@ -0,0 +1,15 @@
+const world_war_2 = "1952-2-23";
+
+console.log(world_war_2);
+
+let favouritemeal = "Rice";
+let favouritePragramming = "Golang";
+
+let Gnum = 56;
+let Pnum = 8.8;
+
+let programmer = true;
+
+let largeNum = "10283888847747u"
+
+let h = null;
